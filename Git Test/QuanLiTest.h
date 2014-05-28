ca77c1da97969f758ab8c86bcbe5d048de704df5
李泉 <quanlidavid@QuanMac.local>
2014-05-28 17:23:00 +0800

    added a test class

---
+++ Git Test/QuanLiTest.h
@@ -0,0 +1,13 @@
+//
+//  QuanLiTest.h
+//  Git Test
+//
+//  Created by 李泉 on 14-5-28.
+//  Copyright (c) 2014年 李泉. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QuanLiTest : NSObject
+
+@end
---
+++ Git Test/QuanLiTest.m
@@ -0,0 +1,13 @@
+//
+//  QuanLiTest.m
+//  Git Test
+//
+//  Created by 李泉 on 14-5-28.
+//  Copyright (c) 2014年 李泉. All rights reserved.
+//
+
+#import "QuanLiTest.h"
+
+@implementation QuanLiTest
+
+@end
